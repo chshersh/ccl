945051a91abbf426a5bc34878e3c82700d9ba147
Dmitrii Kovanikov <kovanikov@gmail.com>
2024-12-06 22:03:52 +0000

    Finish parser!

--- lib/ccl.ml
+++ lib/ccl.ml
@@ -10,4 +10,4 @@ let main () =
   let file = "test.ccl" in
   print_endline "Started parsing...";
   let kvs = read file in
-  ListLabels.iter kvs ~f:(fun (k, v) -> Printf.printf "%s = %s\n%!" k v)
+  ListLabels.iter kvs ~f:(fun (k, v) -> Printf.printf "'%s' = '%s'\n%!" k v)
--- lib/parser.ml
+++ lib/parser.ml
@@ -1,18 +1,49 @@
 open Angstrom
 
-let from_chars chars = chars |> List.to_seq |> String.of_seq |> String.trim
 let space = char ' ' <|> char '\t' >>| fun _ -> ()
 let blank = skip_many (space <|> end_of_line)
-let value_p = many (not_char '\n') >>| from_chars
+let from_chars chars = chars |> List.to_seq |> String.of_seq |> String.trim
+let to_str chars = chars |> List.to_seq |> String.of_seq
+
+let value_p =
+  (* Ignore all leading spaces *)
+  let* _ = many space in
+
+  (* Recursive parser *)
+  let value_line_p =
+    fix (fun self ->
+        let* current_line = many (not_char '\n') in
+        let* opt_char = peek_char in
+
+        match opt_char with
+        (* End of input *)
+        | None -> return current_line (* Return what's parsed *)
+        | Some '\n' -> (
+            let* () = end_of_line in
+            let* spaces = many space in
+            match spaces with
+            (* Next line starts immediately from the start. Aborting. *)
+            | [] -> return current_line
+            (* Some spaces. Continue parsing value. *)
+            | _ ->
+                let* next = self in
+                let prefix = List.init (List.length spaces) (fun _ -> ' ') in
+                (* TODO: Quadratic behaviour; fix it. *)
+                return (current_line @ [ '\n' ] @ prefix @ next))
+        | Some c ->
+            fail
+              ("Error parsing value. Expecting \\n but got: " ^ String.make 1 c))
+  in
+
+  (* Call recursive function *)
+  value_line_p
 
 let key_val =
-  let ( let* ) = ( >>= ) in
   let* _ = blank in
   let* key = many (not_char '=') >>| from_chars in
   let* _ = blank in
   let* _ = char '=' in
-  let* _ = blank in
-  let* value = value_p in
+  let* value = value_p >>| to_str in
   let* _ = blank in
   return (key, value)
 
