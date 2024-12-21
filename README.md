e1bb70a7077c52e4c1e30542f2b8fed8f696d685
Dmitrii Kovanikov <kovanikov@gmail.com>
2024-12-21 11:19:42 +0000

    Add some docs

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# ccl
+# CCL: The most elegant configuration language
 
-Categorical Configuration Language
+CCL stands for **Categorical Configuration Language**.
+
+Example:
+
+```
+/= This is a CCL document
+title = CCL Example
+
+database =
+  enabled = true
+  ports =
+    = 8000
+    = 8001
+    = 8002
+  limits =
+    cpu = 1500mi
+    memory = 10Gb
+
+user =
+  guestId = 42
+
+user =
+  login = chshersh
+  createdAt = 2024-12-31
+```
+
+CCL is just a key-value mapping. Yet, it's powerful enough to support:
+
+1. Key-value mappings (obviously)
+1. Lists
+1. Strings
+1. Dates
+1. Algebraic Data Types
+1. Comments
+1. Sections
+1. Nested records
 
 ## Development
 
--- lib/ccl.mli
+++ lib/ccl.mli
@@ -4,35 +4,31 @@ module Model = Model
 (** Parser of the configuration. *)
 module Parser = Parser
 
-(** This function converts a list of key-value pairs into [value Model.KeyMap.t].
+(** [decode_file path] Read the file contents and runs {!decode} on the result.
+
+Internally, this function parse the string into a list of key-value pairs and
+then recursively parses values.
 
 This function parses values of the input list using the same parser as for the
 top-level config modulo common space prefix. This function continues parsing
 keys recursively and stops only when the parsing doesn't change the output
 (that's why it's a fix-point combinator)
 
-The intended usage is the following:
+Usage example:
 
 {[
 let example () =
-  let filepath = "test.ccl" in
-  let contents = In_channel.with_open_bin filename In_channel.input_all in
+  let filepath = "example.ccl" in
 
-  match Ccl.Parser.parse contents with
-  | Error (`Parse_error error) ->
-    Printf.printf "Error %s\n%!" msg;
-    exit (-1)
-  | Ok key_values ->
-    let ccl = Ccl.fix key_values in
-    ...
+  match Ccl.decode_file filepath with
+  | Error (`Parse_error msg) ->
+    Printf.printf "Error: %s\n%!" msg
+  | Ok ccl ->
+    print_endline (Ccl.Model.pretty ccl)
 ]}
-
-*)
-
-(** Decode contents of a string as [Model.t] or return a parsing failure. *)
-val decode : string -> (Model.t, Parser.error) result
-
-(** Read the file contents and run [decode] on them.
 
 NOTE: This function throws all the exceptions as reading the file. *)
 val decode_file : string -> (Model.t, Parser.error) result
+
+(** Decode contents of a string as [Model.t] or return a parsing failure. *)
+val decode : string -> (Model.t, Parser.error) result
